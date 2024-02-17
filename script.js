fa6db86993dae6cd3430fbb627bb77e8ea9bd365
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2024-02-16 23:11:06 -0300

    Add italic and underline function Change buttons to have titles when hover few style changes

--- script.js
+++ script.js
@@ -3,17 +3,61 @@ var button = document.getElementById("boldbtn");
 var Isclicked = false;
 
 
-
-button.onclick = function turnBold() {
+function turnBold() {
     if(Isclicked)
     {
         document.getElementById("user_input").style.fontWeight = "normal" ;
         Isclicked = false
+        document.getElementById("boldbtn").style.filter = "none"
     }
     else if( Isclicked = true)
     {
         document.getElementById("user_input").style.fontWeight = "bold" ;
         Isclicked = true
+        document.getElementById("boldbtn").style.filter = "drop-shadow(rgb(0, 0, 0) 0.2rem 0.2rem 0.2rem)"
+    }
+}
+function turnItalic() {
+    if(Isclicked)
+    {
+        document.getElementById("user_input").style.fontStyle = "normal" ;
+        Isclicked = false
+        document.getElementById("italicbtn").style.filter = "none"
+    }
+    else if( Isclicked = true)
+    {
+        document.getElementById("user_input").style.fontStyle = "italic" ;
+        Isclicked = true
+        document.getElementById("italicbtn").style.filter = "drop-shadow(rgb(0, 0, 0) 0.2rem 0.2rem 0.2rem)"
+    }
+}
+function turnUnderline() {
+    if(Isclicked)
+    {
+        document.getElementById("user_input").style.textDecoration = "none";
+        Isclicked = false
+        document.getElementById("underlinebtn").style.filter = "none"
+    }
+    else if( Isclicked = true)
+    {
+        document.getElementById("user_input").style.textDecoration = "underline" ;
+        Isclicked = true
+        document.getElementById("underlinebtn").style.filter = "drop-shadow(rgb(0, 0, 0) 0.2rem 0.2rem 0.2rem)"
+    }
+}
+
+function turnUnderline() {
+    if(Isclicked)
+    {
+        document.getElementById("user_input").style.textDecoration = "none";
+        Isclicked = false
+        document.getElementById("underlinebtn").style.filter = "none"
+    }
+    else if( Isclicked = true)
+    {
+        document.getElementById("user_input").style.textDecoration = "underline" ;
+        Isclicked = true
+        document.getElementById("underlinebtn").style.filter = "drop-shadow(rgb(0, 0, 0) 0.2rem 0.2rem 0.2rem)"
     }
 }
 
