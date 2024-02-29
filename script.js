da0feb6cb9bfda0dfbb7ecad968b04e992c92bcd
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2024-02-28 22:21:53 -0300

    finish main program

--- script.js
+++ script.js
@@ -1,6 +1,85 @@
+const textarea = document.getElementById("textarea");
+
+
+function changeFontSize(element){
+    let value = element.value;
+    textarea.style.fontSize = value + "px";
+}
+
+function turnBold(element){
+    if(textarea.style.fontWeight=="bold")
+    {
+        textarea.style.fontWeight="normal";
+        element.classList.remove("active");
+    }
+    else{
+        textarea.style.fontWeight="bold";
+        element.classList.add("active");
+    }
+}
+
+function turnItalic(element){
+    if(textarea.style.fontStyle=="italic")
+    {
+        textarea.style.fontStyle="normal";
+        element.classList.remove("active");
+    }
+    else{
+        textarea.style.fontStyle="italic";
+        element.classList.add("active");
+    }
+}
+
+function turnUnderline(element){
+    if(textarea.style.textDecoration=="underline")
+    {
+        textarea.style.textDecoration="none";
+        element.classList.remove("active");
+    }
+    else{
+        textarea.style.textDecoration="underline";
+        element.classList.add("active");
+    }
+}
+
+function alignLeft(element){
+    textarea.style.textAlign="left"
+}
+function alignCenter(element){
+    textarea.style.textAlign="center"
+}
+function alignRight(element){
+    textarea.style.textAlign="right"
+}
+
+function upper(element){
+    if(textarea.style.textTransform=="uppercase")
+    {
+        textarea.style.textTransform="none";
+        element.classList.remove("active");
+    }
+    else{
+        textarea.style.textTransform="uppercase";
+        element.classList.add("active");
+    }
+}
+
+function resetStyle(element){
+    textarea.style.fontWeight = "normal";
+    textarea.style.fontStyle = "normal";
+    textarea.style.textAlign = "left";
+    textarea.style.fontSize = "16px";
+    textarea.style.textTransform ="capitalize";
+    textarea.value ="";
+    textarea.style.color ="#000";
+}
 
 
 
-function changeFontSize(this){
 
+
+
+function changeColor(element){
+    let value = element.value;
+    textarea.style.color = value ;
 }
